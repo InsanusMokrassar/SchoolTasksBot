aa52bd5eede8c490dcb456ac6379823707bbcd07
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-02-28 17:42:24 +0600

    updates

---
+++ students/courses/src/jvmMain/kotlin/JvmPlugin.kt
@@ -0,0 +1,19 @@
+package center.sciprog.tasks_bot.students.courses
+
+import dev.inmo.micro_utils.fsm.common.State
+import dev.inmo.plagubot.Plugin
+import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContextWithFSM
+import kotlinx.serialization.json.JsonObject
+import org.jetbrains.exposed.sql.Database
+import org.koin.core.Koin
+import org.koin.core.module.Module
+
+object JvmPlugin : Plugin {
+    override fun Module.setupDI(database: Database, params: JsonObject) {
+        with(CommonPlugin) { setupDI(database, params) }
+    }
+
+    override suspend fun BehaviourContextWithFSM<State>.setupBotPlugin(koin: Koin) {
+        with(CommonPlugin) { setupBotPlugin(koin) }
+    }
+}

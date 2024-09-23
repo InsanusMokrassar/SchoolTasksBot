3b2c9c4bfa719a68abc8a52a72619eeea306ba74
InsanusMokrassar <insanus-mokrassar@yandex.ru>
2024-09-24 02:10:52 +0600

    Update BaseRequest.kt

--- webapp/common/src/commonMain/kotlin/models/BaseRequest.kt
+++ webapp/common/src/commonMain/kotlin/models/BaseRequest.kt
@@ -13,8 +13,11 @@ import org.koin.core.module.Module
  * 1. Create [BaseRequest] realization
  * 2. Create result type which will be used as an [R] argument
  * 3. Register your request in plugin with [registerRequestType] in `setupDI`
+ * 4. Add realization of [center.sciprog.tasks_bot.webapp.server.models.RequestHandler]
+ * 5. Register relization of RequestHandler using [center.sciprog.tasks_bot.webapp.server.models.registerRequestHandler] in `setupDI` in commonMain of webapp.server
  *
  * @see StatusRequest
+ * @see center.sciprog.tasks_bot.webapp.server.models.StatusRequestHandler
  */
 interface BaseRequest<R> {
     val resultSerializer: KSerializer<R>

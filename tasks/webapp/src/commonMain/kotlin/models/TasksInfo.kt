1184955c3cdbf5a60f280438280f83bcd066a5e1
y9Kap <wotblitz6allima4ok@gmail.com>
2024-09-30 02:10:06 +0300

    WIP(#add_myroles_request): get my roles request was added, but 401 problem not solved

--- tasks/webapp/src/commonMain/kotlin/models/TasksInfo.kt
+++
@@ -1,8 +0,0 @@
-package center.sciprog.tasks_bot.tasks.webapp.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class TasksInfo(
-    val tasks: List<TaskInfo>,
-)
--- webapp/client/src/jsMain/kotlin/Content.kt
+++ webapp/client/src/jsMain/kotlin/Content.kt
@@ -1,11 +1,15 @@
 package center.sciprog.tasks_bot.webapp.client
 
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import center.sciprog.tasks_bot.common.webapp.DefaultClient
 import center.sciprog.tasks_bot.common.webapp.models.HandlingResult
 import center.sciprog.tasks_bot.common.webapp.models.StatusRequest
 import center.sciprog.tasks_bot.common.webapp.models.status
 import center.sciprog.tasks_bot.tasks.webapp.ui.ActiveTasks
+import center.sciprog.tasks_bot.webapp.client.ui.MyRoles
 import dev.inmo.kslog.common.KSLog
 import dev.inmo.kslog.common.e
 import org.jetbrains.compose.web.dom.Br
@@ -29,8 +33,7 @@ fun WebAppContent(client: DefaultClient) {
     }
 
     Div {
-        val statusValue = status.value
-        when (statusValue) {
+        when (val statusValue = status.value) {
             null -> Text("Status receiving in progress")
             is HandlingResult.Code -> {
                 Text("Problems with status receiving: ${statusValue.code.value}")
@@ -42,6 +45,6 @@ fun WebAppContent(client: DefaultClient) {
             }
         }
     }
-
+    MyRoles(client)
     ActiveTasks(client)
 }

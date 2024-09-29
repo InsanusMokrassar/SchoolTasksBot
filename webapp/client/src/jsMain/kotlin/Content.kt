8e315ed3668162fb8b6a12b061d92f8b9f8f2bf6
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-09-30 00:17:05 +0600

    small refactor of tasks drawer

--- webapp/client/src/jsMain/kotlin/Content.kt
+++ webapp/client/src/jsMain/kotlin/Content.kt
@@ -7,6 +7,7 @@ import center.sciprog.tasks_bot.common.webapp.models.StatusRequest
 import center.sciprog.tasks_bot.common.webapp.models.status
 import center.sciprog.tasks_bot.tasks.webapp.models.GetActiveTasksRequest
 import center.sciprog.tasks_bot.tasks.webapp.models.activeTasks
+import center.sciprog.tasks_bot.tasks.webapp.ui.DrawActiveTasks
 import dev.inmo.kslog.common.KSLog
 import dev.inmo.kslog.common.e
 import dev.inmo.kslog.common.i
@@ -46,39 +47,5 @@ fun WebAppContent(client: DefaultClient) {
         }
     }
 
-
-    val activeTasks = remember { mutableStateOf<HandlingResult<GetActiveTasksRequest.Response>?>(null) }
-
-    LaunchedEffect(client) {
-        runCatching {
-            activeTasks.value = client.activeTasks()
-        }.onFailure {
-            KSLog("Content").e(it)
-            it.printStackTrace()
-        }
-    }
-
-    Div {
-        val activeTasksValue = activeTasks.value
-        when (activeTasksValue) {
-            null -> Text("Status receiving in progress")
-            is HandlingResult.Code -> {
-                Text("Problems with active tasks receiving: ${activeTasksValue.code.value}")
-            }
-            is HandlingResult.Success -> {
-                if (activeTasksValue.data.teachingTasksInfo.isNotEmpty()) {
-                    Div { Text("Active teaching tasks:") }
-                    activeTasksValue.data.teachingTasksInfo.forEach {
-                        Div { Text("${it.course.title}: ${it.task.title}") }
-                    }
-                }
-                if (activeTasksValue.data.studyingTasksInfo.isNotEmpty()) {
-                    Div { Text("Active studying tasks:") }
-                    activeTasksValue.data.studyingTasksInfo.forEach {
-                        Div { Text("${it.course.title}: ${it.task.title}") }
-                    }
-                }
-            }
-        }
-    }
+    DrawActiveTasks(client)
 }

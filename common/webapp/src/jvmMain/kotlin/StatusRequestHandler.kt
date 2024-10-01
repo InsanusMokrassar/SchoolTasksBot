9acbeb916692606202528f6d2963c7e23169cc32
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-10-01 17:40:16 +0600

    add opportunity to use typed result of handling

--- common/webapp/src/jvmMain/kotlin/StatusRequestHandler.kt
+++ common/webapp/src/jvmMain/kotlin/StatusRequestHandler.kt
@@ -7,7 +7,7 @@ import dev.inmo.tgbotapi.types.UserId
 object StatusRequestHandler : RequestHandler {
     override suspend fun ableToHandle(request: BaseRequest<*>): Boolean = request == StatusRequest
 
-    override suspend fun handle(userId: UserId, request: BaseRequest<*>): HandlingResult<*> {
+    override suspend fun handle(userId: UserId, request: BaseRequest<*>): HandlingResult<StatusRequest.Status> {
         val casted = (request as StatusRequest)
         val runtime = Runtime.getRuntime()
         return StatusRequest.Status(
